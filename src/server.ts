b53486e6cce60dad8a58af980ee7c471754cc1f3
Jason Thompson <jason@jasont.us>
2024-11-10 12:42:25 -0500

    setup express server & database

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import path from 'path'
+import express from 'express'
+import connectDB from './db.config.js'
+import cookieParser from 'cookie-parser'
+import routes from './routes/recipe.js'
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api', routes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/dist')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, './', 'client', 'dist', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('Please set to production')
+  })
+}
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
